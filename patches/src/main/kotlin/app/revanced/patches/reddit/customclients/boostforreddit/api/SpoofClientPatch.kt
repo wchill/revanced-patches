1bd407ce89eca0b96050e81227744d50d587e80a
Eric Ahn <wchill1337@gmail.com>
2025-03-05 22:11:51 -0800

    Add option for patching Boost for Reddit user agent

--- patches/src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
@@ -4,38 +4,44 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patches.reddit.customclients.spoofClientPatch
+import app.revanced.patches.reddit.customclients.spoofUserAgentPatch
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
-val spoofClientPatch = spoofClientPatch(redirectUri = "http://rubenmayayo.com") { clientIdOption ->
+val spoofClientPatch = spoofClientPatch() { clientIdOption, redirectUriOption ->
     compatibleWith("com.rubenmayayo.reddit")
-
-    val clientId by clientIdOption
-
     execute {
-        // region Patch client id.
-
+        // region Patch client id and redirect URI.
         getClientIdFingerprint.method.addInstructions(
             0,
             """
-                 const-string v0, "$clientId"
+                 const-string v0, "$clientIdOption"
                  return-object v0
             """,
         )
 
-        // endregion
-
-        // region Patch user agent.
-
-        // Use a random user agent.
-        val randomName = (0..100000).random()
-        val userAgent = "$randomName:app.revanced.$randomName:v1.0.0 (by /u/revanced)"
-        buildUserAgentFingerprint.let {
-            val userAgentTemplateIndex = it.stringMatches!!.first().index
-            val register = it.method.getInstruction<OneRegisterInstruction>(userAgentTemplateIndex).registerA
-            
-            it.method.replaceInstruction(userAgentTemplateIndex, "const-string v$register, \"$userAgent\"")
+        listOf(loginActivityOnCreateFingerprint, loginActivityAShouldOverrideUrlLoadingFingerprint).forEach { fingerprint ->
+            fingerprint.method.let {
+                fingerprint.stringMatches!!.forEach { match ->
+                    val register = it.getInstruction<OneRegisterInstruction>(match.index).registerA
+                    it.replaceInstruction(match.index, "const-string v$register, \"$redirectUriOption\"")
+                }
+            }
         }
 
+        // endregion
+    }
+}
+
+val userAgentPatch = spoofUserAgentPatch() { userAgentOption ->
+    compatibleWith("com.rubenmayayo.reddit")
+    execute {
+        // region Patch user agent.
+        buildUserAgentFingerprint.method.let {
+            buildUserAgentFingerprint.stringMatches!!.forEach { match ->
+                val register = it.getInstruction<OneRegisterInstruction>(match.index).registerA
+                it.replaceInstruction(match.index, "const-string v$register, \"$userAgentOption\"")
+            }
+        }
         // endregion
     }
 }
