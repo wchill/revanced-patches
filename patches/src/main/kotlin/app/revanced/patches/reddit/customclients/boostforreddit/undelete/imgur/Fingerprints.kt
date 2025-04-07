5e4b197d2b150c5028fd47f2ecdd8e1e88ab177e
Eric Ahn <wchill1337@gmail.com>
2025-04-06 22:42:53 -0700

    Add support for fetching Imgur/Reddit images from Wayback Machine (closes #3, #4)

---
+++ patches/src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/undelete/imgur/Fingerprints.kt
@@ -0,0 +1,12 @@
+package app.revanced.patches.reddit.customclients.boostforreddit.undelete.imgur
+
+import app.revanced.patcher.fingerprint
+
+
+internal val installImgurPaidOkHttpInterceptorFingerprint = fingerprint {
+    custom { method, _ -> method.name == "d" && method.definingClass == "Lbc/a;" }
+}
+
+internal val installImgurFreeOkHttpInterceptorFingerprint = fingerprint {
+    custom { method, _ -> method.name == "e" && method.definingClass == "Lbc/a;" }
+}
